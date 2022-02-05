3292289d041bb94fd3f79ca05a44816a7a13858f
Jack Eadie <jeadi@amazon.com>
2022-02-05 14:09:01 +1000

    always run callbank functions for ALL topic

--- src/queue.ts
+++ src/queue.ts
@@ -7,14 +7,17 @@ export default class Queue<T> {
   allTopic: string = "ALL"; // Listen to all topics.
 
   publish = (topic: string, ...args: T[]) => {
-    if (this.callbackFns[topic]) {
-      // For each subscription, call fn with each argument from args.
-      this.callbackFns[topic].forEach(fn => {
-        args.map(x => {
-          fn(x);
-        });
+    let callbacks =
+      this.callbackFns[topic] || this.callbackFns[this.allTopic]
+        ? this.callbackFns[topic].concat(this.callbackFns[this.allTopic])
+        : [];
+
+    // For each subscription, call fn with each argument from args.
+    callbacks.forEach(fn => {
+      args.map(x => {
+        fn(x);
       });
-    }
+    });
   };
 
   subscribe = (topic: string, fn: (x: T) => any) => {
